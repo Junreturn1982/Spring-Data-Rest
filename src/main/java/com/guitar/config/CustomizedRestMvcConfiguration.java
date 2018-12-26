538bbca5ac7c1448d0bef1155d40b8f124dbd763
junreturn1982 <ngochoang071982@gmail.com>
2018-12-26 21:57:04 +0700

    Spring boot JPA

---
+++ src/main/java/com/guitar/config/CustomizedRestMvcConfiguration.java
@@ -0,0 +1,21 @@
+package com.guitar.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+
+@Configuration
+public class CustomizedRestMvcConfiguration extends  RepositoryRestMvcConfiguration {
+	
+	@Value("${pathbaseUri}")
+	private String pathbaseUri;
+
+	@Override
+	public RepositoryRestConfiguration config() {
+		RepositoryRestConfiguration config = super.config();
+		config.setBaseUri(pathbaseUri);
+		return config;
+	}
+
+}
